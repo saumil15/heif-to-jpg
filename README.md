94dd93f4e964a05238db79bdeabfe509ed34537c
Saumil Thaker <saumil.thaker@nexergroup.com>
2024-09-09 15:14:44 +0200

    This code will convert multiple heif to jpg.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# heif-to-jpg
-This is an online application used for converting iphone heif images to jpg.
+# HEIF to JPEG Converter
+
+This is a web-based application built using Flask that allows users to upload HEIF/HEIC images and convert them to JPEG format.
+
+## Running the Application
+
+### Prerequisites
+
+- Docker
+- Python 3.10
+
+### Steps to Run:
+
+1. **Clone the repository**
+2. **docker build -t heif-converter.**
+3. **docker run -p 5000:5000 -v ${PWD}\uploads:/app/uploads -v ${PWD}\converted:/app/converted heif-converter**
+
+This project will only run on linux or mac as pyheif module doesn't work on windows. if you wish to run on windows use docker image
+
---
+++ app.py
@@ -0,0 +1,75 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory, flash
+from werkzeug.utils import secure_filename
+from PIL import Image
+import pyheif
+
+UPLOAD_FOLDER = 'uploads'
+CONVERTED_FOLDER = 'converted'
+ALLOWED_EXTENSIONS = {'heif', 'heic'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['CONVERTED_FOLDER'] = CONVERTED_FOLDER
+app.secret_key = 'supersecretkey'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+if not os.path.exists(CONVERTED_FOLDER):
+    os.makedirs(CONVERTED_FOLDER)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def heif_to_jpg(heif_path, jpg_path):
+    heif_file = pyheif.read(heif_path)
+    image = Image.frombytes(
+        heif_file.mode,
+        heif_file.size,
+        heif_file.data,
+        "raw",
+        heif_file.mode,
+        heif_file.stride,
+    )
+    image.save(jpg_path, "JPEG")
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'files' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        files = request.files.getlist('files')
+        converted_files = []
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                heif_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(heif_path)
+                jpg_filename = os.path.splitext(filename)[0] + '.jpg'
+                jpg_path = os.path.join(app.config['CONVERTED_FOLDER'], jpg_filename)
+                try:
+                    heif_to_jpg(heif_path, jpg_path)
+                    converted_files.append(jpg_filename)
+                except Exception as e:
+                    flash(f'Conversion failed for {filename}: {e}')
+                    return redirect(request.url)
+        flash('Files successfully converted')
+        return redirect(url_for('show_converted_files', filenames=converted_files))
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/converted/<filename>')
+def converted_file(filename):
+    return send_from_directory(app.config['CONVERTED_FOLDER'], filename)
+
+@app.route('/converted_files')
+def show_converted_files():
+    filenames = request.args.getlist('filenames')
+    return render_template('converted_files.html', filenames=filenames)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
